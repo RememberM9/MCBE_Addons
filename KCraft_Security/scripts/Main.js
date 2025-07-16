1649b2e226f783186a5a5e5eecba243c20ea8418
Remember M9 <109746665+RememberM9@users.noreply.github.com>
2025-07-17 01:17:56 +0800

    voided

--- KCraft_Security/scripts/Main.js
+++
@@ -1,4 +0,0 @@
-import 'scripts/KCraftTests/IllegalEnchantmentSet.js';
-import 'scripts/KCraftTests/X_Items.js';
-import 'scripts/KCraftTests/EntityFilters.js';
-import 'scripts/KCraftTests/_EXPERIMENTS.js';
